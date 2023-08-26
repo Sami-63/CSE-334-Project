34c3a29dff839fbe16f30806906adbf545910ec9
Sami-63 <rssami.bd@gmail.com>
2023-08-27 00:02:09 +0600

    full

--- frontend-mui/src/App.jsx
+++ frontend-mui/src/App.jsx
@@ -1,27 +1,3 @@
-import "@fontsource/roboto/300.css";
-import "@fontsource/roboto/400.css";
-import "@fontsource/roboto/500.css";
-import "@fontsource/roboto/700.css";
-
-// import { Container, Typography, Button } from "@mui/material";
-// import { makeStyles } from "@mui/styles";
-
-// const useStyles = makeStyles((theme) => ({
-//   root: {
-//     display: "flex",
-//     flexDirection: "column",
-//     alignItems: "center",
-//     justifyContent: "center",
-//     minHeight: "100vh",
-//   },
-//   title: {
-//     marginBottom: theme.spacing(2),
-//   },
-//   button: {
-//     marginTop: theme.spacing(2),
-//   },
-// }));
-
 const App = () => {
   return <h1>hi</h1>;
 };
--- frontend/src/page/LoginPage.jsx
+++ frontend/src/page/LoginPage.jsx
@@ -1,26 +1,23 @@
-import { Alert, Button, Col, Form, Row } from "react-bootstrap";
-// eslint-disable-next-line no-unused-vars
-import { Link, useLocation, useNavigate } from "react-router-dom";
-import FormContainer from "../conponents/FormContainer";
 import { useEffect, useState } from "react";
-
+import { Alert, Button, Col, Form, Row } from "react-bootstrap";
+import { Link, useNavigate } from "react-router-dom";
 import useLogin from "../hooks/useLogin";
 import { useAuthContext } from "../hooks/useAuthContext";
+import { IoMailOutline, IoLockClosedOutline } from "react-icons/io5";
+import "./LoginPage.css";
 
 const LoginPage = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
   const { login, isLoading, error } = useLogin();
+  const navigate = useNavigate();
+  const { user } = useAuthContext();
 
   const submitHandler = async (e) => {
     e.preventDefault();
-
     await login(email, password);
   };
-
-  const navigate = useNavigate();
-  const { user } = useAuthContext();
 
   useEffect(() => {
     if (user) {
@@ -28,49 +25,84 @@ const LoginPage = () => {
     }
   }, [navigate, user]);
 
+  const images = [
+    "https://img.freepik.com/free-photo/beautiful-luxury-outdoor-swimming-pool-hotel-resort_74190-7433.jpg",
+    "https://img.freepik.com/free-photo/beautiful-silhouette-palm-tree-with-umbrella-chair_74190-2099.jpg?w=360",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSf7SDYpiNXfebX1h2_wNgCYAXh-wig0E7FQ9nFMrsNWDvfel-Id_D4WMPMDqtw3ZpGv6E&usqp=CAU",
+  ];
+
+  const [activeImageIndex, setActiveImageIndex] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setActiveImageIndex((prevIndex) => (prevIndex + 1) % images.length);
+    }, 3000); // Change image every 1 second
+
+    return () => clearInterval(interval);
+  }, []);
+
   return (
-    <FormContainer>
-      <h1 className='text-center'>Sign In</h1>
-      {error && <Alert variant='danger'>{error}</Alert>}
+    <div className='login-container'>
+      <div className='image-container'>
+        {images.map((imageUrl, index) => (
+          <img
+            src={imageUrl}
+            alt={`Image ${index}`}
+            className={activeImageIndex === index ? "active" : ""}
+            key={index}
+          />
+        ))}
+      </div>
+      <div className='form-container'>
+        <h1>Sign In</h1>
+        {error && <Alert variant='danger'>{error}</Alert>}
 
-      <Form onSubmit={submitHandler}>
-        <Form.Group controlId='email'>
-          <Form.Label>Email Address</Form.Label>
-          <Form.Control
-            type='email'
-            placeholder='Enter email'
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            required
-          ></Form.Control>
-        </Form.Group>
-        <Form.Group controlId='password'>
-          <Form.Label>Password</Form.Label>
-          <Form.Control
-            type='password'
-            placeholder='Enter password'
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            required
-          ></Form.Control>
-        </Form.Group>
+        <Form onSubmit={submitHandler}>
+          <Form.Group controlId='email'>
+            <Form.Label className='form-label'>
+              <IoMailOutline className='form-icon' /> Email Address
+            </Form.Label>
+            <Form.Control
+              type='email'
+              placeholder='Enter email'
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+            />
+          </Form.Group>
+          <Form.Group controlId='password'>
+            <Form.Label className='form-label'>
+              <IoLockClosedOutline className='form-icon' /> Password
+            </Form.Label>
+            <Form.Control
+              type='password'
+              placeholder='Enter password'
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />
+          </Form.Group>
 
-        <Button
-          type='submit'
-          variant='primary'
-          disabled={isLoading}
-          className='my-3'
-        >
-          Sign In
-        </Button>
-      </Form>
+          <Button
+            type='submit'
+            variant='primary'
+            className='login-button'
+            disabled={isLoading}
+          >
+            Sign In
+          </Button>
+        </Form>
 
-      <Row>
-        <Col>
-          New User? <Link to='/register'>Register</Link>
-        </Col>
-      </Row>
-    </FormContainer>
+        <Row className='py-3'>
+          <Col>
+            New User?{" "}
+            <Link to='/register' className='register-link'>
+              Register
+            </Link>
+          </Col>
+        </Row>
+      </div>
+    </div>
   );
 };
 
